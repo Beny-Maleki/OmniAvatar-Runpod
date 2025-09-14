dafab8cf4be29e39dc795941f7a208e718218b0d
benyamin <benyaminmaleki81@gmail.com>
2025-09-14 09:33:03 +0330

    Remove flash attention

--- builder/download_models.py
+++ builder/download_models.py
@@ -1,4 +1,4 @@
-from huggingface_hub import snapshot_download, hf_hub_download
+from huggingface_hub import snapshot_download
 
 if __name__ == "__main__":
     print("Downloading models from Hugging Face Hub...")
@@ -17,15 +17,6 @@ if __name__ == "__main__":
     snapshot_download(
         repo_id="OmniAvatar/OmniAvatar-14B",
         local_dir="./pretrained_models/OmniAvatar-14B",
-        local_dir_use_symlinks=False
-    )
-
-    # Download the specific FlashAttention wheel
-    hf_hub_download(
-        repo_id="alexnasa/flash-attn-3",
-        repo_type="model",
-        filename="128/flash_attn_3-3.0.0b1-cp39-abi3-linux_x86_64.whl",
-        local_dir="./flash_attn_wheel",
         local_dir_use_symlinks=False
     )
 
